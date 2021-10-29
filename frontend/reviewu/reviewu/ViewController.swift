a7fbf0fc49fdc78ab60fe6359a12071a3213f6b0
semples <semples@umich.edu>
2021-10-29 11:11:26 -0400

    added frontend files

---
+++ frontend/reviewu/reviewu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reviewu
+//
+//  Created by Spencer Semple on 10/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
